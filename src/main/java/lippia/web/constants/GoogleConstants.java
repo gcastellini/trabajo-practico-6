10086390ba5e5f8fa6c3467422ac0f713697ad08
Giuliana <giuliana_castellini@hotmail.com>
2022-10-04 22:10:47 -0300

    second commit

--- src/main/java/lippia/web/constants/GoogleConstants.java
+++ src/main/java/lippia/web/constants/GoogleConstants.java
@@ -4,7 +4,7 @@ public class GoogleConstants {
 
     public static final String INPUT_SEARCH_XPATH = "xpath://input[@class='gLFyf gsfi']";
     public static final String SEARCH_BUTTON_NAME = "xpath://*[@id='menu-item-40']";
-    public static final String STATS_ID = "id:rcnt";
+    public static final String STATS_ID = "xpath://*[@id='themify_builder_content-22']/div[2]/div/div/div/div/div[2]/div[%s]";
 
     public  static final String HOME_BUTTON_NAME= "xpath://*[@id='content']/nav/a";
 
--- src/main/java/lippia/web/services/GoogleSearchResultService.java
+++ src/main/java/lippia/web/services/GoogleSearchResultService.java
@@ -2,21 +2,31 @@ package lippia.web.services;
 
 import com.crowdar.core.actions.ActionManager;
 import lippia.web.constants.GoogleConstants;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import java.util.List;
+import java.util.Objects;
 
 
 public class GoogleSearchResultService extends ActionManager {
 
-    private static WebElement stats() {
-        return getElement(GoogleConstants.STATS_ID);
+    public static List<WebElement> stats() {
+        List<WebElement> allElements = getElements(GoogleConstants.STATS_ID,String.valueOf(1));
+        for (int i = 2; i <= 3; i++) {
+            allElements.add(getElement(GoogleConstants.STATS_ID, String.valueOf(i)));
+        }
+        return allElements;
     }
 
-    public static String getStats() {
-        return stats().getText();
+    public static int getStats() {
+        return stats().size();
     }
 
     public static void verifyResults(){
-        Assert.assertFalse(getStats().isEmpty());
+        Assert.assertEquals(getStats(), 3);
     }
+
+
 }
